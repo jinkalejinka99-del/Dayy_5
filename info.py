e603db6618ffe8a78bf96120140f9fa9f44cf10f
jinkalejinka99-del <jinkalejinka99@gmail.com>
2026-03-27 13:23:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,10 @@
 import os
 import socket
 import datetime
+import getpass
 
 print("=== SYSTEM INFO ===")
 print("Time:", datetime.datetime.now())
 print("Hostname:", socket.gethostname())
 print("Current Files:", os.listdir())
-print("User:", os.getlogin())
+print("User:", getpass.getuser())
